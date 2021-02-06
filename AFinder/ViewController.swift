9af694c2b678f97aaa93284c53495ca008fe32f5
Daniel Tejeda <juandanieltd@gmail.com>
2021-02-05 21:17:00 -0700

    Initial Commit

---
+++ AFinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AFinder
+//
+//  Created by Daniel Tejeda on 05/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
