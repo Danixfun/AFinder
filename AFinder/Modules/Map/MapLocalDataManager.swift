894ca0069085b85be334cb8a2b02beed660b536d
Daniel Tejeda <juandanieltd@gmail.com>
2021-02-07 17:53:51 -0700

    chore(onboarding): clean onboarding project

---
+++ AFinder/Modules/Map/MapLocalDataManager.swift
@@ -0,0 +1,15 @@
+//
+//  MapLocalDataManager.swift
+//  AFinder
+//
+//  Created by Daniel Tejeda on 07/02/21.
+//  
+//
+
+import Foundation
+
+class MapLocalDataManager:MapLocalDataManagerInputProtocol {
+    
+    var localRequestHandler: MapLocalDataManagerOutputProtocol?
+    
+}
---
+++ AFinder/Modules/Map/MapPresenter.swift
@@ -0,0 +1,28 @@
+//
+//  MapPresenter.swift
+//  AFinder
+//
+//  Created by Daniel Tejeda on 07/02/21.
+//  
+//
+
+import Foundation
+
+class MapPresenter  {
+    
+    // MARK: Properties
+    weak var view: MapViewProtocol?
+    var interactor: MapInteractorInputProtocol?
+    var wireFrame: MapWireFrameProtocol?
+    
+}
+
+extension MapPresenter: MapPresenterProtocol {
+    // TODO: implement presenter methods
+    func viewDidLoad() {
+    }
+}
+
+extension MapPresenter: MapInteractorOutputProtocol {
+    // TODO: implement interactor output methods
+}
